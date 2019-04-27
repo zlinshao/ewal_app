443b256274c6ca8c38c7c5e75c3a3984d7a7ebef
shuisheng <njshuisheng@126.com>
2019-04-27 18:57:35 +0800

    to be continued

--- config/index.js
+++ config/index.js
@@ -13,8 +13,8 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    // host: 'localhost', // can be overwritten by process.env.HOST
-    host: '192.168.20.66', // can be overwritten by process.env.HOST
+    host: 'localhost', // can be overwritten by process.env.HOST
+    // host: '192.168.1.110', // can be overwritten by process.env.HOST
     port: 8085, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
---
+++ src/components/pages/houseResource/expand-container.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="expand-container" :style="myStyle" ref="expandContainer" @click.self="handleCloseExpand">
+    <div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "index",
+    props: ['offsetTop'],
+    data() {
+      return {
+        myStyle: {
+          height: 0,
+          top: 0,
+        },
+      }
+    },
+    mounted() {},
+    watch: {
+      offsetTop(top) {
+        console.log(top);
+        if (top) {
+          if (typeof top !== 'number') {
+            return false;
+          }
+          this.myStyle.top = top + 'px';
+          this.myStyle.height = window.innerHeight - top + 'px';
+        } else {
+          this.myStyle.height = 0;
+        }
+      },
+    },
+    computed: {},
+    methods: {
+      handleCloseExpand() {
+        this.myStyle.height = 0;
+        this.$emit('close');
+      },
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+  #expand-container {
+    width: 100%;
+    background-color: rgba(0,0,0,.3);
+    position: absolute;
+    left: 0;
+    top: 133px;
+    z-index: 99;
+    transition: all ease .3s;
+    overflow: hidden;
+    > div {
+      background-color: white;
+      min-height: 300px;
+    }
+  }
+</style>
