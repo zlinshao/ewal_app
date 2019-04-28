77c425dcd6baca45f8b46d2c8400b7904075e7de
shuisheng <njshuisheng@126.com>
2019-04-28 16:21:04 +0800

    to be continued

--- config/index.js
+++ config/index.js
@@ -13,12 +13,8 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    // host: 'localhost', // can be overwritten by process.env.HOST
-<<<<<<< HEAD
-    host: '192.168.1.110', // can be overwritten by process.env.HOST
-=======
-    host: '192.168.20.69', // can be overwritten by process.env.HOST
->>>>>>> e6632a5df5577767212d5a64ee3dcec050612ac5
+    host: 'localhost', // can be overwritten by process.env.HOST
+    // host: '192.168.1.110', // can be overwritten by process.env.HOST
     port: 8085, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
