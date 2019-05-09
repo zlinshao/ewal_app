7a270e87a461b2648789e8d2ddfb931dbccb9833
linshao <601267015@qq.com>
2019-05-09 10:43:31 +0800

    fff

--- src/components/pages/punchClock/index.vue
+++ src/components/pages/punchClock/index.vue
@@ -183,9 +183,9 @@
       // 确定打卡
       finishPunchClock() {
         this.postForm.variables = this.jsonClone(this.form);
-        this.form.variables.property_phone = '18052001167';
-        this.form.variables.remark = '发的啥开发圣诞快乐';
-        this.form.variables.look_photo = [4227577, 4227578, 4227579];
+        this.postForm.variables.property_phone = '182052501756';
+        this.postForm.variables.remark = '发的啥开发圣诞快乐';
+        this.postForm.variables.look_photo = [4227577, 4227578, 4227579];
         this.$httpZll.postFinishPunchClock(this.postForm).then(res => {
           if (res.success) {
             this.close_();
