312ac6534677ae66c21430448aa260ed19c21c38
linshao <601267015@qq.com>
2019-06-10 02:51:42 +0800

    'fff'

--- src/utils/fun.prototype.js
+++ src/utils/fun.prototype.js
@@ -741,7 +741,6 @@ export default {
             this.$prompt('获取部门失败!', 'fail');
             return;
           }
-          console.log(data);
           this.$store.dispatch('personal_storage', data);
           resolve(true);
         }
