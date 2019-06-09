e591802c1032fe630cd2e670f004de0216fa5953
linshao <601267015@qq.com>
2019-06-09 21:35:11 +0800

    'fff'

--- src/config/boss.config.js
+++ src/config/boss.config.js
@@ -6,7 +6,6 @@ globalConfig = {
   server_hr: 'http://test.hr.api.ewal.lejias.cn/',//人资接口
   server_identity: 'http://test.e_contract.api.ewal.lejias.cn/',//身份认证
   server_done: 'http://47.101.204.100:8080/',//小飞 接口
-  // server_done:'http://192.168.20.64:8080/',//小飞 接口 本地
   domain: 'http://static.lejias.cn/',
-  token: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjQ2ZDRjMDk2ZDllZWNhMzlkNGJmZjM3NzdjNzIxNDM0Yzc4YWJkNTk5OWJmNjkwZTM1OTRkZWQxZGZiYmRjZmNiZTI5MWQ1ODhmNWM0NTYxIn0.eyJhdWQiOiIxIiwianRpIjoiNDZkNGMwOTZkOWVlY2EzOWQ0YmZmMzc3N2M3MjE0MzRjNzhhYmQ1OTk5YmY2OTBlMzU5NGRlZDFkZmJiZGNmY2JlMjkxZDU4OGY1YzQ1NjEiLCJpYXQiOjE1NTk5NjAxOTEsIm5iZiI6MTU1OTk2MDE5MSwiZXhwIjoxNTYxMjU2MTkxLCJzdWIiOiI2OSIsInNjb3BlcyI6W119.rMlCeXYCYaWMNEYqF-3b95ulKbZ3K23SHP_cv8s1B2WURk_AP9yeehNcoNBZCp5fMz0tLRYMufDcxGFebbNCvlu2meGWyxu3Ja-fpQv4IsqVLZN5rWcewJmicPQMGhCB1KEl5kEJ453qm71PU96w399eNwLRlOmyfbPKYVozTjEmn-x0St_gogywL9BxikdNIXWAq7psXaLRZM5lFEnOlkwj6U0ZsSFwjrhFxoFf2ym52DJlSvt1RRGrMnnpVdV21o7dY9Dj4FMnrTKKjDX_OiyYMp3CVaa4y4ayb5riNXcK52C3BbS8mYIXFLHIPVF2zenCssCjEvFpmSefN6R-q4GpS8ZvJ9czgVwVwKQnHXep5-DZ2mTwTGawQvf1oZFbV2HJbNjjyD6BD6L20zTpJnRzhqIoJNLad1qjxUGIRCKdBEdh0ApGd-xdyLmTpdiIbXJiS-HcQpxxWwiVXmI07A2iw13uAY7Y6Hrtqydd12x800GcYr-E97s-VWvNKU5tJb7yq3FnCe_XCPEKFpLud1qfmY13BT7O9gjSecQD7mbM6rouWqvVh55H-y2tJPmVhv2O7ODl1C0B6Neq_9ISsGn1HSmGSs65ALCmFxgjGyS9M37Iufj7ptxQto8jrOqLZWfV3vvYQ2gelc-BuABPyD8jgM3fiMGApwS5OA2bQqQ',
+  token: '',
 };
