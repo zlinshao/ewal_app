562c2ad5f8fa6ae9b3fb8c3cac2330ef4f0385df
linshao <601267015@qq.com>
2019-06-20 22:42:26 +0800

    'fff'

--- src/components/pages/toBeDoneList/components/noFinish.vue
+++ src/components/pages/toBeDoneList/components/noFinish.vue
@@ -25,13 +25,13 @@
       <div class="moduleMain">
         <div class="main">
           <div class="detail">
-            <div v-for="item in Object.keys(showFormat)" v-if="allDetail[item] || item === 'remaining_time'">
+            <div v-for="item in Object.keys(showFormat)" v-if="allDetail[item]">
               <label>{{showFormat[item]}}</label>
-              <span v-if="item === 'remaining_time'">
-                <span class="unit">{{allDetail.due_date_hours}}<b>h</b></span>
-                <span class="unit">{{allDetail.due_date_minutes}}<b>m</b></span>
-              </span>
-              <span v-else-if="item === 'house_goods'">
+              <!--<span v-if="item === 'remaining_time'">-->
+              <!--  <span class="unit">{{allDetail.due_date_hours}}<b>h</b></span>-->
+              <!--  <span class="unit">{{allDetail.due_date_minutes}}<b>m</b></span>-->
+              <!--</span>-->
+              <span v-if="item === 'house_goods'">
                 <b v-if="allDetail[item]">
                   <span v-for="house in Object.keys(allDetail[item])" style="display: block;">
                     <span v-if="house !== 'is_fill'">缺少{{makeGoods[house]}}{{allDetail[item][house]}}个</span>
@@ -39,7 +39,7 @@
                   </span>
                 </b>
               </span>
-<!--              <span v-else>{{allDetail[item] || ''}}</span>-->
+              <span v-else>{{allDetail[item] || ''}}</span>
             </div>
           </div>
           <div class="record" v-for="item in recordList">
@@ -92,7 +92,7 @@
           house_address: '房屋地址',
           house_goods: '待办内容',
           due_date: '结束时间',
-          remaining_time: '剩余时间',
+          // remaining_time: '剩余时间',
           bulletin_staff_name: '跟进人',
         },
         makeGoods: {
