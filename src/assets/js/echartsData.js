a3cec807eed120a93fa0ee3401369d58fa19b7a4
linshao <601267015@qq.com>
2019-07-04 15:37:12 +0800

    '111'

--- src/assets/js/echartsData.js
+++ src/assets/js/echartsData.js
@@ -1,110 +1,28 @@
-{
-  [
-    {
-      "IOPSRead": 1,
-      "IntranetBandwidth": 1760,
-      "BPSWrite": 743936,
-      "IntranetTX": 21360,
-      "IntranetRX": 84504,
-      "InstanceId": "i-uf6233f281xkuyw00vhx",
-      "CPU": 11,
-      "TimeStamp": "2019-04-15T05:53:00Z",
-      "BPSRead": 261120,
-      "InternetRX": 232746,
-      "IOPSWrite": 3,
-      "InternetTX": 42893
-    },
-    {
-      "IOPSRead": 1,
-      "IntranetBandwidth": 3048,
-      "BPSWrite": 413696,
-      "IntranetTX": 32512,
-      "IntranetRX": 150544,
-      "InstanceId": "i-uf6233f281xkuyw00vhx",
-      "CPU": 12,
-      "TimeStamp": "2019-04-15T05:54:00Z",
-      "BPSRead": 274944,
-      "InternetRX": 153319,
-      "IOPSWrite": 3,
-      "InternetTX": 28551
-    },
-    {
-      "IOPSRead": 1,
-      "IntranetBandwidth": 1280,
-      "BPSWrite": 390144,
-      "IntranetTX": 18232,
-      "IntranetRX": 58576,
-      "InstanceId": "i-uf6233f281xkuyw00vhx",
-      "CPU": 11,
-      "TimeStamp": "2019-04-15T05:51:00Z",
-      "BPSRead": 263680,
-      "InternetRX": 255972,
-      "IOPSWrite": 2,
-      "InternetTX": 46127
-    },
-    {
-      "IOPSRead": 1,
-      "IntranetBandwidth": 3112,
-      "BPSWrite": 519680,
-      "IntranetTX": 33600,
-      "IntranetRX": 153664,
-      "InstanceId": "i-uf6233f281xkuyw00vhx",
-      "CPU": 12,
-      "TimeStamp": "2019-04-15T05:52:00Z",
-      "BPSRead": 261632,
-      "InternetRX": 191401,
-      "IOPSWrite": 3,
-      "InternetTX": 19617
-    },
-    {
-      "IOPSRead": 1,
-      "IntranetBandwidth": 1752,
-      "BPSWrite": 455680,
-      "IntranetTX": 25776,
-      "IntranetRX": 78696,
-      "InstanceId": "i-uf6233f281xkuyw00vhx",
-      "CPU": 12,
-      "TimeStamp": "2019-04-15T05:58:00Z",
-      "BPSRead": 275968,
-      "IOPSWrite": 3
-    },
-    {
-      "IntranetBandwidth": 1656,
-      "IntranetTX": 25008,
-      "IntranetRX": 74928,
-      "InstanceId": "i-uf6233f281xkuyw00vhx",
-      "CPU": 12,
-      "TimeStamp": "2019-04-15T05:55:00Z",
-      "InternetRX": 60642,
-      "InternetTX": 15177
-    },
-    {
-      "IOPSRead": 1,
-      "IntranetBandwidth": 320,
-      "BPSWrite": 764928,
-      "IntranetTX": 7672,
-      "IntranetRX": 11896,
-      "InstanceId": "i-uf6233f281xkuyw00vhx",
-      "CPU": 11,
-      "TimeStamp": "2019-04-15T05:56:00Z",
-      "BPSRead": 266240,
-      "InternetRX": 48297,
-      "IOPSWrite": 4,
-      "InternetTX": 14992
-    },
-    {
-      "IOPSRead": 2,
-      "IntranetBandwidth": 1712,
-      "BPSWrite": 421888,
-      "IntranetTX": 26280,
-      "IntranetRX": 77416,
-      "InstanceId": "i-uf6233f281xkuyw00vhx",
-      "CPU": 12,
-      "TimeStamp": "2019-04-15T05:57:00Z",
-      "BPSRead": 273408,
-      "InternetRX": 130751,
-      "IOPSWrite": 2,
-      "InternetTX": 25147
+hhhhhhhhhhhh = [
+  {
+    "name": "一级主管审核",
+    "assginees": [{
+      "id": 354,
+      "name": "林婳婳",
+      "avatar": "https://static.dingtalk.com/media/lADPACOG83UJN5_NAu7NAuw_748_750.jpg"
     }
-  ]
-}
+    ]
+  },
+  {
+    "name": "二级主管审核",
+    "assginees": [{
+      "id": 354,
+      "name": "林婳婳",
+      "avatar": "https://static.dingtalk.com/media/lADPACOG83UJN5_NAu7NAuw_748_750.jpg"
+    },
+      {"id": 1117, "name": "许伟兵", "avatar": "https://static.dingtalk.com/media/lADPBbCc1e1aU_bNAYTNAYQ_388_388.jpg"}]
+  },
+  {
+    "name": "三级主管审核",
+    "assginees": [{
+      "id": 354,
+      "name": "林婳婳",
+      "avatar": "https://static.dingtalk.com/media/lADPACOG83UJN5_NAu7NAuw_748_750.jpg"
+    }, {"id": 1117, "name": "许伟兵", "avatar": "https://static.dingtalk.com/media/lADPBbCc1e1aU_bNAYTNAYQ_388_388.jpg"}]
+  }, {"name": "法务审核", "assginees": []},
+  {"name": "行政审核", "assginees": []}]
