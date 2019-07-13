a77094944c32d3f266f11283ec94613e7ecba873
linshao <601267015@qq.com>
2019-07-13 21:27:38 +0800

    'fff'

--- src/utils/fun.prototype.js
+++ src/utils/fun.prototype.js
@@ -1036,7 +1036,7 @@ export default {
       data.avatar = info.avatar;
       data.phone = info.phone;
       data.staff_id = info.id;
-      data.staff_id = '';
+      // data.staff_id = '';
       data.staff_name = info.name;
       data.org = info.org;
       if (info.company_info) {
